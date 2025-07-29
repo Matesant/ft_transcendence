07fad8a78f12f68a7827c49ef92f5dc2b1c0dd51
Mateus santos <mateussantos1528@hotmail.com>
2025-07-28 23:25:58 -0300

    cors

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
 
 module.exports = {
     entry: './src/app.ts', //path to the main .ts file
@@ -36,9 +35,6 @@ module.exports = {
         new HtmlWebpackPlugin({
             inject: true,
             template: 'public/index.html',
-        }),
-        new webpack.DefinePlugin({
-            'process.env.IP': JSON.stringify(process.env.IP)
         }),
     ],
     devtool : 'inline-source-map',
