7d014af0b0ff8f3c29dcc423735321e86ae090f9
Alisson <alissonmarcos249@gmail.com>
2025-07-28 22:04:55 -0300

    dinamismo no start.sh no frontend

--- frontend/start.sh
+++ frontend/start.sh
@@ -2,11 +2,12 @@
 
 while true; do
   if [[ -f "package.json" && -d "src" ]]; then
-    npm i
-    npm run build
-    npx tailwindcss -i src/style.css -o public/style.css
+    if ! grep -qs bundle.js public/index.html; then
+      npm i
+      npm run build
+      npx tailwindcss -i src/style.css -o public/style.css
+    fi
     exec nginx -g "daemon off;"
-    break
   else
     echo "Aguardando arquivo package.json e pasta src..."
     sleep 1
